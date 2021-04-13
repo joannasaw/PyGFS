834ecde0659246bc0795f97fe845c465bfb8fdb1
Sean Lew <seanlewtengsiong@gmail.com>
2021-04-14 01:07:58 +0800

    added clear buttons for gui

--- CURRENT/gui.py
+++ CURRENT/gui.py
@@ -19,22 +19,28 @@ except:
 
 
 def clearDisplay():
-    txtarea.delete("1.0", "end")
+    txtarea.config(state=NORMAL)
+    txtarea.delete("1.0", END)
+    txtarea.config(state=DISABLED)
 
 
 def clearFileEntry():
     filenameEntry.delete(0, 'end')
 
 
+def clearEditEntry():
+    contentEntry.delete(0, 'end')
+
+
 def refreshAllFiles():
     for label in list(frame1.children.values()):
         label.destroy()
 
+    global files
     files = master.get_list_of_files()
-    print(files)
     for i, each_file in enumerate(files):
         label = Button(frame1,
-                       text=each_file,
+                       text=os.path.splitext(each_file)[0],
                        command=lambda file_name=each_file: readFile(file_name)).pack(side=TOP, fill=BOTH)
     Button(
         frame1,
@@ -52,12 +58,14 @@ def openFile():
         filetypes=(("Text Files", "*.txt"),)
     )
     filenameEntry.delete(0, 'end')
-    filenameEntry.insert(END, tf)
+
     tf = open(tf)
 
     # set global filename variable for uploadFile() function to use
     global filename
     filename = os.path.split(tf.name)[1]
+    filename = os.path.splitext(filename)[0]
+    filenameEntry.insert(END, filename)
     file_cont = tf.read()
     txtarea.config(state=NORMAL)
     txtarea.delete("1.0", "end")
@@ -73,19 +81,38 @@ def readFile(file_name):
     txtarea.delete("1.0", "end")
     txtarea.insert(END, full_data)
     txtarea.config(state=DISABLED)
+    clearFileEntry()
+    filenameEntry.insert(0, file_name)
+    clearEditEntry()
 
 
 def uploadFile():
-    # print(filename)
-    success = put(master, filename, filename)
-
-    if success:
+    print(filename)
+    file_name = filenameEntry.get()
+    file_content = txtarea.get("1.0", END)
+    if len(str(file_name)) == 0:
+        messagebox.showinfo('Error', "Please enter filename")
+    elif len(str(file_name)) > 20:
         messagebox.showinfo(
-            'Upload Success', "File successfully uploaded to Server")
-        refreshAllFiles()
-        clearDisplay()
+            'Error', "File name cannot exceed 20 characters")
+    elif file_name in list(files):
+        messagebox.showinfo(
+            'Error', "File name already exist on server!")
+    elif len(str(file_content)) == 0:
+        messagebox.showinfo('Error', "Please enter content of file")
     else:
-        messagebox.showinfo('Upload Fail', "Error uploading file to Server")
+        success = create(master, file_content, file_name)
+
+        if success:
+            messagebox.showinfo(
+                'Upload Success', "File successfully uploaded to Server")
+            refreshAllFiles()
+            clearDisplay()
+            clearFileEntry()
+            clearEditEntry()
+        else:
+            messagebox.showinfo(
+                'Upload Fail', "Error uploading file to Server")
 
 
 def saveFile():
@@ -103,12 +130,16 @@ def saveFile():
 
 def deleteFile():
     file_name = filenameEntry.get()
+    if len(str(file_name)) == 0:
+        messagebox.showinfo('Error', "Please enter filename")
     success = delete(master, file_name)
     if success:
         messagebox.showinfo(
             'Delete Success', 'File Successfully deleted from Server')
         refreshAllFiles()
         clearDisplay()
+        clearFileEntry()
+        clearEditEntry()
     else:
         messagebox.showinfo(
             'Delete Failure', 'Error deleting file from Server')
@@ -116,20 +147,27 @@ def deleteFile():
 
 def createFile():
     file_name = filenameEntry.get()
+    if len(str(file_name)) == 0:
+        messagebox.showinfo('Error', "Please enter filename")
     file_content = txtarea.get("1.0", END)
-
+    if len(str(file_content)) == 0:
+        messagebox.showinfo('Error', "Please enter content of file")
     success = create(master, file_content, file_name)
     if success:
         messagebox.showinfo(
             'Create Success', 'File successfully created on Server')
         refreshAllFiles()
         clearDisplay()
+        clearFileEntry()
+        clearEditEntry()
     else:
         notice.config(text="Error creating file on Server")
 
 
 def appendFile():
     file_name = filenameEntry.get()
+    if len(str(file_name)) == 0:
+        messagebox.showinfo('Error', "Please enter filename")
     content_to_append = str(contentEntry.get())
     print("content:", content_to_append)
     success = write_append(master, content_to_append, file_name)
@@ -139,6 +177,7 @@ def appendFile():
         clearDisplay()
         contentEntry.delete(0, "end")
         readFile(file_name)
+        clearEditEntry()
     else:
         messagebox.showinfo('Append Failure', 'Error Appending to File')
 
@@ -161,41 +200,40 @@ Button(
     text="Refresh",
     command=refreshAllFiles,
 ).pack(side=BOTTOM)
-# adding scrollbars
-# ver_sb = Scrollbar(frame, orient=VERTICAL)
-# ver_sb.pack(side=RIGHT, fill=BOTH)
-
-# hor_sb = Scrollbar(frame, orient=HORIZONTAL)
-# hor_sb.pack(side=BOTTOM, fill=BOTH)
-
 
 # adding display space
 txtarea = Text(frame, width=60, height=20)
 txtarea.pack(side=LEFT)
-# txtarea.config(state=DISABLED)
 
-
-# binding scrollbar with text area
-# txtarea.config(yscrollcommand=ver_sb.set)
-# ver_sb.config(command=txtarea.yview)
-
-# txtarea.config(xscrollcommand=hor_sb.set)
-# hor_sb.config(command=txtarea.xview)
 frame2 = Frame(ws)
 frame2.pack(fill=X, ipady=10)
 # adding path showing box
 filenameLabel = Label(frame2, text="File Name", width=10)
 filenameLabel.pack(side=LEFT, padx=5, pady=5)
-filenameEntry = Entry(frame2)
-filenameEntry.pack(expand=True, fill=X, padx=5)
-
+frame5 = Frame(frame2)
+frame5.pack(expand=True, fill=X)
+filenameEntry = Entry(frame5, borderwidth=1)
+filenameEntry.pack(expand=True, fill=X, side=LEFT, padx=5)
+Button(
+    frame5,
+    text="Clear",
+    command=clearFileEntry
+).pack(padx=5)
 frame3 = Frame(ws)
 frame3.pack(fill=X)
 
+
 contentLabel = Label(frame3, text="Edit", width=10)
 contentLabel.pack(side=LEFT, padx=5, pady=5)
-contentEntry = Entry(frame3)
-contentEntry.pack(expand=True, fill=X, padx=5)
+frame6 = Frame(frame3)
+frame6.pack(expand=True, fill=X)
+contentEntry = Entry(frame6, borderwidth=1)
+contentEntry.pack(expand=True, fill=X, side=LEFT, padx=5)
+Button(
+    frame6,
+    text="Clear",
+    command=clearEditEntry
+).pack(padx=5)
 
 # Fetch all the available files on the server once at the start
 refreshAllFiles()
@@ -221,12 +259,6 @@ Button(
     text="Create",
     command=createFile
 ).pack(side=LEFT, expand=True, fill=X, padx=10)
-
-# Button(
-#     ws,
-#     text="Read",
-#     command=readFile
-# ).pack(side=LEFT, expand=True, fill=X, padx=20, pady=20)
 
 Button(
     frame4,
